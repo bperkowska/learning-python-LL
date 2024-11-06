7321d3c13a45376ad2e16244c5316d28d2b79954
sme-perk003 <berenika.perkowska@sonymusic.com>
2024-11-06 15:43:37 +0100

    "playling with VS code (reset after)

--- Ch2 - Basics/variables_finished.py
+++ Ch2 - Basics/variables_finished.py
@@ -9,7 +9,7 @@ myint = 5
 myfloat = 13.2
 mystr = "This is a string"
 mybool = True
-mylist = [0, 1, "two", 3.2, False]
+zmieninonalista = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
@@ -17,7 +17,7 @@ print(myint)
 print(myfloat)
 print(mystr)
 print(mybool)
-print(mylist)
+print(zmieninonalista)
 print(mytuple)
 print(mydict)
 
@@ -26,12 +26,12 @@ myint = "abc"
 print (myint)
 
 # to access a member of a sequence type, use []
-print(mylist[2])
+print(zmieninonalista[2])
 print(mytuple[1])
 # use slices to get parts of a sequence
-print(mylist[1:4:2])
+print(zmieninonalista[1:4:2])
 # you can use slices to reverse a sequence
-print(mylist[::-1])
+print(zmieninonalista[::-1])
 
 # dictionaries are accessed via keys
 print(mydict["one"]) 
@@ -43,11 +43,18 @@ print ("string type " + str(123))
 # Global vs. local variables in functions
 def someFunction():
     #global mystr
-    mystr = "def"
-    print (mystr)
+    zmienionanazwa = "def"
+    print ( zmienionanazwa )
 
 someFunction()
 print (mystr) 
 
-del mystr
+def new_func(mystr):
+    del mystr
+
+new_func(mystr)
 print (mystr)
+
+zmieninonalista.__getattribute__()
+
+function
